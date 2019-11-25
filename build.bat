3efdd077599b5c3bdd408d04b182c84b42694ae8
Daniel Bok <daniel.bok@outlook.com>
2019-11-25 12:05:48 +0800

    Update windows batch file build

--- build.bat
+++ build.bat
@@ -6,11 +6,6 @@ make clean
 set PKG_DIR=%CD%
 set NLOPT_DIR=%CD%\nlopt_src
 
-@rem Set PYTHON Version
-FOR /F "tokens=* USEBACKQ" %%G IN (`python -c "import sys; s = sys.version_info; print('{0}{1}'.format(s[0], s[1]))"`) DO (
-    set PY_VER=%%G
-)
-
 @rem install numpy, needed to build python
 CALL conda install -y -v numpy twine
 
@@ -19,12 +14,11 @@ rmdir /S /Q %NLOPT_DIR%
 git clone https://github.com/stevengj/nlopt %NLOPT_DIR%
 
 @rem move into build folder
-cd %NLOPT_DIR%
-mkdir build && cd build
+cd %NLOPT_DIR% && mkdir build && cd build
 
 @rem Release configuration
 set CONFIG=Release
-set CMAKE_PLATFORM=Visual Studio 15 2017 Win64
+set CMAKE_PLATFORM=Visual Studio 16 2019
 
 @rem build package
 set INST_DIR=%NLOPT_DIR%\install
@@ -38,7 +32,7 @@ cp %INST_DIR%\lib\site-packages\_nlopt.pyd %PKG_DIR%\nlopt\_nlopt.pyd
 cp %INST_DIR%\lib\site-packages\nlopt.py %PKG_DIR%\nlopt\nlopt.py
 
 cd %PKG_DIR%
-python setup.py bdist_wheel --python-tag cp%PY_VER% --py-limited-api cp%PY_VER%m --plat-name win_amd64
+python setup.py bdist_wheel --plat-name win_amd64
 
 @rem upload to pypi
 @rem twine upload --skip-existing dist/*
