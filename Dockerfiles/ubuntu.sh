c324055c8c937fc08fdf4fe6c6eb8af197c363f4
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-04 11:45:22 +0200

    overhaul

--- Dockerfiles/ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-versions=(3.7 3.8)
-user=danielbok
-
-for ver in "${versions[@]}"; do
-  docker image build -t ${user}/ubuntu:${ver} -f ubuntu.Dockerfile --build-arg PY_VER=${ver} .
-done
