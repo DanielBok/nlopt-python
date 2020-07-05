063daba6733371e03193df9e4b8053e760c93ff4
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-05 16:46:43 +0200

    fix

--- scripts/install_swig.sh
+++ scripts/install_swig.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+yum update -y && yum install pcre-devel -y
+
 cd /tmp
-curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download --output swig.tar.gz
+if [ ! -e swig.tar.gz ]; then # helps during debugging...
+  curl -L https://sourceforge.net/projects/swig/files/swig/swig-4.0.2/swig-4.0.2.tar.gz/download --output swig.tar.gz
+fi
+
 rm -rf /tmp/swig
 mkdir swig
+
 tar -xvzf /tmp/swig.tar.gz -C swig --strip-components 1
 cd swig
-./configure && make && make install
+./configure --without-alllang --with-python3 && make -j2 && make install
