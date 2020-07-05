d1bd7f2b929ae6be57d8aa2df9cbf69ccd9e0222
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-05 17:16:57 +0200

    better

--- scripts/install_swig.sh
+++ scripts/install_swig.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-yum update -y && yum install pcre-devel -y
+if [ ! -e /tmp/swig.tar.gz ]; then
+  # only do this once
 
-cd /tmp
-if [ ! -e swig.tar.gz ]; then # helps during debugging...
-  curl -L https://sourceforge.net/projects/swig/files/swig/swig-4.0.2/swig-4.0.2.tar.gz/download --output swig.tar.gz
+  curl -L https://sourceforge.net/projects/swig/files/swig/swig-4.0.2/swig-4.0.2.tar.gz/download --output /tmp/swig.tar.gz
 fi
 
-rm -rf /tmp/swig
-mkdir swig
+if [ ! -e /tmp/swig ]; then
+  # only do this once
 
-tar -xvzf /tmp/swig.tar.gz -C swig --strip-components 1 &> /dev/null
-cd swig
-./configure --without-alllang --with-python3 && make -j2 && make install
+  yum update -y && yum install pcre-devel -y
+
+  mkdir /tmp/swig
+  tar -xvzf /tmp/swig.tar.gz -C /tmp/swig --strip-components 1 &> /dev/null
+
+  pushd /tmp/swig
+  ./configure --without-alllang --with-python3 && make -j2 && make install > /dev/null
+  popd
+fi
