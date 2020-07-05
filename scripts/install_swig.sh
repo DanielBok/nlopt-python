d07cfe778c8be277674475fed34f5422e830a9e8
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-05 15:55:01 +0200

    override nlopt detection

---
+++ scripts/install_swig.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+yum update -y && yum install pcre-devel -y
+
+cd /tmp
+if [ ! -e swig.tar.gz ]; then # helps during debugging...
+  curl -L https://sourceforge.net/projects/swig/files/swig/swig-4.0.2/swig-4.0.2.tar.gz/download --output swig.tar.gz
+fi
+
+rm -rf /tmp/swig
+mkdir swig
+
+tar -xvzf /tmp/swig.tar.gz -C swig --strip-components 1 &> /dev/null
+cd swig
+./configure --without-alllang --with-python3 && make -j2 && make install
