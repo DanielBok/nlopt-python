f12dcace6fbaf5c724daeb9276246f32d9f9dd4b
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-05 16:24:05 +0200

    fix

--- scripts/install_swig.sh
+++ scripts/install_swig.sh
@@ -3,6 +3,6 @@ cd /tmp
 curl -L https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download --output swig.tar.gz
 rm -rf /tmp/swig
 mkdir swig
-tar -xvzf /var/swig.tar.gz -C swig --strip-components 1
+tar -xvzf /tmp/swig.tar.gz -C swig --strip-components 1
 cd swig
 ./configure && make && make install
