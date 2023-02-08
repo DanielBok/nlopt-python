0b2e1c120b8b8245bae89d2b868e298d0998061a
Daniel Bok <daniel.bok@outlook.com>
2023-02-08 21:41:04 +0800

    update setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(Path(__file__).parent / "extern" / "nlopt" / "CMakeLists.txt") as f:
 
 setup(
     version=version,
-    install_requires=["numpy >=1.14"],
+    install_requires=["numpy >=1.18.5"],
     ext_modules=[NLOptBuildExtension("nlopt._nlopt", version)],
     cmdclass={"build_ext": NLOptBuild},
     zip_safe=False,
