ad0a48b66f78e76f18095ab0649dd9f02dab86c4
Julien Schueller <schueller@phimeca.com>
2024-02-08 17:17:37 +0100

    Try to build with stable API

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from setuptools import setup
 
 from extensions import NLOptBuild, NLOptBuildExtension
 
+from wheel.bdist_wheel import bdist_wheel
+
 with open("README.md") as f:
     long_description = f.read()
 
@@ -16,10 +18,23 @@ with open(Path(__file__).parent / "extern" / "nlopt" / "CMakeLists.txt") as f:
         version.append(m.group(1))
     version = ".".join(version)
 
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.6
+            return "cp36", "abi3", plat
+
+        return python, abi, plat
+
+
 setup(
     version=version,
     install_requires=["numpy >=1.18.5"],
     ext_modules=[NLOptBuildExtension("nlopt._nlopt", version)],
-    cmdclass={"build_ext": NLOptBuild},
+    cmdclass={"build_ext": NLOptBuild,
+              "bdist_wheel": bdist_wheel_abi3},
     zip_safe=False,
 )
